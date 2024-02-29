1b6d13bec59b1d69ceae233a909d754b1b3b2907
marla-gsng <marlaisabelle.geesing@gmail.com>
2024-02-29 13:51:15 +0100

    Completed Task 1

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -5,9 +5,12 @@ import './App.css'
 
 function App() {
    const [todoItems, setTodoItems] = useState([
-    { task: 'Learn React', status: 'open', id: '01' },
-    { task: 'Build a project', status: 'open', id: '02' },
-    { task: 'Deploy project', status: 'open', id: '03' }
+    // { task: 'Learn React', status: 'open', id: '01' },
+    // { task: 'Build a project', status: 'open', id: '02' },
+    // { task: 'Deploy project', status: 'open', id: '03' },
+    // { task: 'Delete Project', status: 'open', id: '04' },
+    // { task: 'Create a new practice project', status: 'open', id: '05' },
+
   ]);
 
   return (
--- to-do-list/src/List.jsx
+++ to-do-list/src/List.jsx
@@ -4,7 +4,12 @@
 const List = ({todoItems}) => {
       return (
     <div className="list">
-    
+        <h2>On my list are:</h2>
+         {todoItems.length === 0 ? (
+        <p>No tasks today!</p>
+      ) 
+      : 
+      (
       <ul>
         {todoItems.map(todo => (
           <li key={todo.id}>
@@ -12,10 +17,9 @@ const List = ({todoItems}) => {
           </li>
         ))}
       </ul>
-    </div>
+      )}
+      </div>
   );
 }
-
-console.log(List);
 
 export default List
